99a299da1dcc4d63dad262ecca2d464a600d06ba
Amarico <Amarico@equipo-zalman>
2018-09-10 19:24:36 +0200

    // Muestra los números del 320 al 160, contando de 20 en 20 hacia atrás   //utilizando un bucle for.

---
+++ src/numeroshaciaatrasconfor/NumerosHaciaAtrasConFor.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numeroshaciaatrasconfor;
+
+/**
+ *
+ * @author Amarico
+ */
+public class NumerosHaciaAtrasConFor {
+
+  /**
+   * @param args the command line arguments
+   */
+  
+  // Muestra los números del 320 al 160, contando de 20 en 20 hacia atrás
+  //utilizando un bucle for.
+  
+  
+  public static void main(String[] args) {
+    // TODO code application logic here
+    
+    
+    // Inicio bucle for
+    
+    //
+    
+    for (int i = 320; i >= 160; i-=20) {
+      
+      System.out.println(i);
+      
+    }
+  }
+  
+}
